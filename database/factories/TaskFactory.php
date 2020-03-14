344a6abf5cdecd8dfbd33b89258ee6f953bee009
unknown <gabriel.santana@gmail.com>
2020-03-14 05:53:38 -0300

    Created Task model. Created TaskTest

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Task;
+use Faker\Generator as Faker;
+
+$factory->define(Task::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->paragraph,
+        'user_id' => factory('App\User')->create()->id,
+    ];
+});
